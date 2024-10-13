84bb762fedce37eb99d6d59233d6cb163e03c042
agilizetotal <vendas.agilizetotal@gmail.com>
2024-10-12 21:21:19 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("jornada de dados")
+
+# variáveis
+idade = 34
+nome = "luciano"
+
+# print
+print(idade)
+
+# soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# lista
+lista = [1, 2, 3, 4, 5]
+
+# métodos
+# criando uma nova lista invertida sem alterar a original
+lista_invertida = lista[::-1]
+print(lista_invertida)
